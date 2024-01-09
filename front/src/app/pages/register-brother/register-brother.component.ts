6482317bc980776c855df870ea6a06143799a1c2
MadalenaCampos <madacampos02@gmail.com>
2024-01-09 10:19:02 -0300

    pagina resgistro

---
+++ front/src/app/pages/register-brother/register-brother.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-brother',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-brother.component.html',
+  styleUrl: './register-brother.component.less',
+})
+export class RegisterBrotherComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
