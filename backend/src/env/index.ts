21aabd0415f31809381570a87a12e2532291e1ba
Nidsonr1 <nidsoncampos@hotmail.com>
2024-01-09 12:42:36 -0300

    Configuração do back

---
+++ backend/src/env/index.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+	NODE_ENV: z.enum(['dev', 'homolog', 'production']).default('dev'),
+	API_PORT: z.coerce.number(),
+	DB_USERNAME: z.coerce.string(),
+	DB_PASSWORD: z.coerce.string(),
+	DB_DATABASE: z.coerce.string(),
+	DB_PORT: z.coerce.number(),
+	ACESS_KEY: z.string(),
+	DATABASE_URL: z.string(),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+	console.error('❎ - Invalid environment variables', _env.error.format());
+  
+	throw new Error('invalid environment variables');
+}
+
+export const env = _env.data;
