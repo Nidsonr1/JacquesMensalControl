8855ae1143b1af293938e499c0dcaa4278950c88
MadalenaCampos <madacampos02@gmail.com>
2024-01-09 15:01:44 -0300

    refatorando frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PagesModule } from './pages/pages.module';
 
 @Component({
   selector: 'jmc-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PagesModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
 })
