846bb70ab14549292ab7401cf740b68eb85963de
Nidsonr1 <nidsoncampos@hotmail.com>
2024-01-09 11:45:16 -0300

    Remove backend

--- backend/src/repositories/lodge-repository.ts
+++
@@ -1,8 +0,0 @@
-import { Lodge } from "entities/lodge";
-
-export interface LodgeRepository {
-  create(data: Lodge): Promise<void>;
-  findById(id: string): Promise<Lodge | null>;
-  findByName(name: string): Promise<Lodge | null>;
-  list(): Promise<Lodge[] | null>;
-}
--- backend/src/repositories/role-repository.ts
+++
@@ -1,7 +0,0 @@
-import { Role } from "entities/role";
-
-export interface RoleRepository {
-  create(data: Role): Promise<void>;
-  findByName(name: string): Promise<Role | null>;
-  findById(id: string): Promise<Role | null>;
-}
